148685a6ea9336bf289d22103125004944da4078
newronsoft <newronsoft@newronsofts-iMacSY.local>
2017-01-05 17:44:54 +0900

    2017/01/05 5:44

--- Projectreview/accountapp/views.py
+++ Projectreview/accountapp/views.py
@@ -20,7 +20,7 @@ def login_view(request):
 	
 def logout_view(request):
 	logout(request)
-	return redirect('/accountapp/loginview/')
+	return redirect('/account/loginview/')
 
 def signup_view(request):
 	if request.method=="POST":
